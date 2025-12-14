6ccb7bd04e157cda4c43d3e20aca5fe3331919ca
wyntomp <wynjessangel.tompong@nmsc.edu.ph>
2025-12-14 17:40:52 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-
+// src/main.jsx (or src/index.js if using Create React App)
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.js';
+import './index.css'; // Global or base styles
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
